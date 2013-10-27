b0aabd5bf49c40022d504db8c307b1cd69be5ada
Cycling Engineer <paul-github@silly-science.co.uk>
2013-10-27 23:29:47 +0000

    Add OK/FAIL response, unit ID/code. More error checking and bit of a tidy up.

--- JeelinkHaStick/JeelinkHaStick.ino
+++ JeelinkHaStick/JeelinkHaStick.ino
@@ -3,6 +3,18 @@
 
 #define MAX_SERIAL_CMD_LEN  20
 
+#define TRANSMITTER_CMD_LEN  3
+static char TRANSMITTER_OK[TRANSMITTER_CMD_LEN] = {0x03,0x00,0x01};
+static char TRANSMITTER_FAIL[TRANSMITTER_CMD_LEN] = {0x03,0x00,0x00};
+
+#define CMD_HEADER_LEN 6
+
+void send_transmitter_status( uint8_t state )
+{
+    if (state) Serial.write(TRANSMITTER_OK, TRANSMITTER_CMD_LEN);
+    else Serial.write(TRANSMITTER_FAIL, TRANSMITTER_CMD_LEN);
+}
+
 void process_system_cmd( char *cmd )
 {
   
@@ -10,15 +22,17 @@ void process_system_cmd( char *cmd )
 
 void process_salus_rt500rf_cmd( char *cmd )
 {
+    char *payload = (cmd+CMD_HEADER_LEN);
+    
     /* Payload format:
      * BYTE#  Description
-     * 2      0x1 = On, 0x0 = Off
+     * 0      0x1 = On, 0x0 = Off
      */
-    if (cmd[2] == 0x1 || cmd[2] == 0x0)
+    if (payload[0] == 0x1 || payload[0] == 0x0)
     {
-      jeelinkha_salusrt500_onOff( (uint8_t)cmd[2] );
-      Serial.println("OK");
-    } else Serial.println("ERR");
+      jeelinkha_salusrt500_onOff( (uint8_t)cmd[4] );
+      send_transmitter_status(1); // OK
+    } else send_transmitter_status(0); // FAIL
 }
 
 void setup() {
@@ -36,20 +50,24 @@ void loop() {
    * BYTE#  Description
    * 0      Pkt Len (include length byte)
    * 1      Command
-   * 2..N   Payload (byte N is (pkt_len - 1))
+   * 2..4   Sensor ID
+   * 5      Unit code
+   * 6..N   Payload (byte N is (pkt_len - 1))
    */
-  while (Serial.readBytes(p_cmd, 1) == 0);
+  while (Serial.readBytes(p_cmd, 1) == 0);  
   Serial.readBytes((p_cmd+1), (int)cmd[0]);
   
-  switch (cmd[1])
-  {
-     case 0x00:
-       process_system_cmd(p_cmd);
-       break;
-     case 0x01:
-       process_salus_rt500rf_cmd(p_cmd);
-       break;
-     default:
-       break;
-  }
+  if (cmd[0] < CMD_HEADER_LEN+1) send_transmitter_status(0); // packets less than CMD_HEADER_LEN+1 bytes are meaningless as they have no payload
+  else
+    switch (cmd[1])
+    {
+      case 0x00:
+        process_system_cmd(p_cmd);
+        break;
+      case 0x01:
+        process_salus_rt500rf_cmd(p_cmd);
+        break;
+      default:
+        break;
+    }
 }
